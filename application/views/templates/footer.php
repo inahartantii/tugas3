c59e4006ecf167929207227234e7e31eee819b33
inahartantii <inahartanti123@gmail.com>
2020-06-04 12:48:13 +0700

    project ketiga

---
+++ application/views/templates/footer.php
@@ -0,0 +1,5 @@
+<script src="http://localhost/codeigniter/assets/js/jquery-3.3.1.slim.min.js"></script>
+<script src="http://localhost/codeigniter/asets/js/pooper.min.js"></script>
+<script src="http://localhost/js"></script>
+</body>
+</html>
